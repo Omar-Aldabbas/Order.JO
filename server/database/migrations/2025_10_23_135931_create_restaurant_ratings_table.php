7671eb6844cd3100f268dc27c0b6b161d0cc4d61
Omar Aldabbas <omaraldabbassjo@gmail.com>
2025-10-24 10:34:05 -0700

    New db schema changes and table added for menuitem varient

--- server/database/migrations/2025_10_23_135931_create_restaurant_ratings_table.php
+++ server/database/migrations/2025_10_23_135931_create_restaurant_ratings_table.php
@@ -6,20 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('restaurant_ratings', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('restaurant_id')->constrained()->onDelete('cascade');
-            $table->foreignId('order_id')->constrained()->onDelete('cascade');
-            $table->tinyInteger('rating')->unsigned();
-            $table->text('review')->nullable();
-            
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('restaurant_ratings');
